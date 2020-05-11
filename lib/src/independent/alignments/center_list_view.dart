e0334ad5fceaf71725d8725c36199eae33787ce7
Vicky Mohammad <vimoh1213@gmail.com>
2020-05-11 18:59:07 -0400

    update center list view widget

--- lib/src/independent/alignments/center_list_view.dart
+++ lib/src/independent/alignments/center_list_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CenterListView extends StatelessWidget {
-  final bool isCenter;
   final Widget child;
   final EdgeInsetsGeometry listViewPadding;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final ScrollPhysics physics;
 
   /// A [ListView] for showing content
   /// center vertically. Takes a [child] [Widget]
@@ -12,17 +14,18 @@ class CenterListView extends StatelessWidget {
   /// on the [child].
   const CenterListView({
     Key key,
-    this.isCenter = true,
     this.listViewPadding,
+	this.mainAxisAlignment = MainAxisAlignment.center,
+	this.crossAxisAlignment = CrossAxisAlignment.center,
+	this.physics,
     @required this.child,
-  }) : super(key: key);
+  }) : assert(child != null, 'CenterListView(): Child cannot be null.'), 
+	  super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(child: LayoutBuilder(builder: (context, constraint) {
-      Widget child = this.child;
-      if (this.isCenter)
-        child = Container(
+      Widget child = Container(
             height: constraint.biggest.height,
             width: constraint.biggest.width,
             child: Column(
@@ -31,7 +34,7 @@ class CenterListView extends StatelessWidget {
                 children: <Widget>[this.child]));
       return ListView(
           padding: this.listViewPadding,
-          physics:
+          physics: this.physics ??
               AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
           children: <Widget>[child]);
     }));
