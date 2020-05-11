1a34c94fb32135ce2ae3b9128468f217cde09623
Vicky Mohammad <vimoh1213@gmail.com>
2020-05-11 19:14:28 -0400

    added controller on list view center param

--- lib/src/independent/alignments/center_list_view.dart
+++ lib/src/independent/alignments/center_list_view.dart
@@ -1,38 +1,42 @@
 import 'package:flutter/material.dart';
 
+/// A list view for showing content
+/// center vertically. Takes a [child] widget
+/// that will be shown in the middle of the list view.
 class CenterListView extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry listViewPadding;
   final MainAxisAlignment mainAxisAlignment;
   final CrossAxisAlignment crossAxisAlignment;
   final ScrollPhysics physics;
+  final ScrollController controller;
 
-  /// A [ListView] for showing content
-  /// center vertically. Takes a [child] [Widget]
+  /// A list view for showing content
+  /// center vertically. Takes a [child] widget
   /// that will be shown in the middle of the list view.
-  /// If you want to have more content, you can pass [Column]
-  /// on the [child].
   const CenterListView({
     Key key,
     this.listViewPadding,
-	this.mainAxisAlignment = MainAxisAlignment.center,
-	this.crossAxisAlignment = CrossAxisAlignment.center,
-	this.physics,
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.physics,
+    this.controller,
     @required this.child,
-  }) : assert(child != null, 'CenterListView(): Child cannot be null.'), 
-	  super(key: key);
+  })  : assert(child != null, 'CenterListView(): Child cannot be null.'),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(child: LayoutBuilder(builder: (context, constraint) {
       Widget child = Container(
-            height: constraint.biggest.height,
-            width: constraint.biggest.width,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[this.child]));
+          height: constraint.biggest.height,
+          width: constraint.biggest.width,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[this.child]));
       return ListView(
+          controller: this.controller,
           padding: this.listViewPadding,
           physics: this.physics ??
               AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
